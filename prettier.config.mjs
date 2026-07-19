b59b708f5701943280c5165031ee4a4068f74341
Amir Salimi <cupofgamehub@gmail.com>
2026-07-19 18:41:54 +0330

    chore: configure linting and formatting

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2
+};
+
+export default config;
